64a22fd5e4fc68e62203e155b6b6528233e75380
Amit Karsale <amit.karsale@perforce.com>
2024-07-25 00:35:00 +0530

    (PA-6139) Drop beaker-puppet_install_helper gem in augeas-core module

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,7 +1,6 @@
 require 'beaker-rspec'
 require 'beaker-puppet'
 require 'beaker/module_install_helper'
-require 'beaker/puppet_install_helper'
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 RSpec.configure do |c|
@@ -42,6 +41,30 @@ r/OSmbaz5mEP0oUA51Aa5BuVnRmhuZyxm7EAHu/QD09CbMkKvO5D+jpxpchNJqU1
 gKDWHrO8Dw9TdSmq6hN35N6MgSGtBxBHEa2HPQfRdbzP82Z+
 -----END CERTIFICATE-----
 EOM
+
+      def run_puppet_install_helper
+        return unless ENV['PUPPET_INSTALL_TYPE'] == 'agent'
+        if ENV['BEAKER_PUPPET_COLLECTION'].match? %r{/-nightly$/}
+          # Workaround for RE-10734
+          options[:release_apt_repo_url] = 'http://nightlies.puppet.com/apt'
+          options[:win_download_url] = 'http://nightlies.puppet.com/downloads/windows'
+          options[:mac_download_url] = 'http://nightlies.puppet.com/downloads/mac'
+        end
+
+        agent_sha = ENV['BEAKER_PUPPET_AGENT_SHA'] || ENV['PUPPET_AGENT_SHA']
+        if agent_sha.nil? || agent_sha.empty?
+          install_puppet_agent_on(hosts, options.merge(version: version))
+        else
+          # If we have a development sha, assume we're testing internally
+          dev_builds_url = ENV['DEV_BUILDS_URL'] || 'http://builds.delivery.puppetlabs.net'
+          install_from_build_data_url('puppet-agent', "#{dev_builds_url}/puppet-agent/#{agent_sha}/artifacts/#{agent_sha}.yaml", hosts)
+        end
+
+        # XXX install_puppet_agent_on() will only add_aio_defaults_on when the
+        # nodeset type == 'aio', but we don't want to depend on that.
+        add_aio_defaults_on(hosts)
+        add_puppet_paths_on(hosts)
+      end
 
       # Until solaris gets new image we need to add to the cert chain on solaris, call a beaker-puppet setup script to handle this
       hosts.each do |host|
